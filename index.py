da63218d1802ac083f9207210e7fcc4d67aa9a84
Leonardo Nascimento <leonardofernandespro@gmail.com>
2025-07-07 16:23:31 -0300

    Realizado a implementação da confirmação da existência de dados no arquivo json

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 import json
+import re
+import os
 
 """
 cadastro-clientes-python
@@ -6,14 +8,30 @@ cadastro-clientes-python
 
 clientes = []
 id_atual = 1
-index = 1
+
+if os.path.exists('clientes.json'):
+    try:
+        with open('clientes.json', 'r', encoding='utf-8') as f:
+            clientes = json.load(f)
+            if clientes:
+                id_atual = max(cliente['Id'] for cliente in clientes) + 1
+    except Exception as e:
+        print(f"Erro ao carregar dados: {e}")
 
 def adicionarCliente():
 	global id_atual
 	nome_cliente = input("Informe o nome do novo cliente: ")
 	idade_cliente = int(input("Informe a idade do novo cliente: "))
 	email_cliente = input("Informe o e-mail do novo cliente: ")
-	
+
+	while True:
+		pattern = r'^[\w\.-]+@[a-zA-Z\d-]+\.[a-zA-Z]{2,}$'
+		if re.match(pattern, email_cliente):
+			break
+		else:
+			print(f'{email_cliente} is incorrect')
+			email_cliente = input("Informe o e-mail do novo cliente: ")
+		
 	novo_cliente = {
 	"Nome": nome_cliente,
 	"Idade": idade_cliente,
@@ -52,12 +70,15 @@ def deletarCliente():
 			print(f"Cliente {cliente['Nome']} do E-mail: {cliente['E-mail']} deletado.")
 	clientes = [cliente for cliente in clientes if not cliente["Status"]]
 	
+
+
+	
 	
 	
 def iniciar():
 	
 	while True:
-		inicio = int(input(f"Escolha uma opção abaixo: \n\n[ 1 ] - Adicionar cliente \n[ 2 ] - Listar clientes \n[ 3 ] - Atualizar dados do cliente \n[ 4 ] - Deletar cliente \n[ 5 ] - Sair\n\n"))
+		inicio = int(input(f"Escolha uma opção abaixo: \n\n[ 1 ] - Adicionar cliente \n[ 2 ] - Listar clientes \n[ 3 ] - Atualizar dados do cliente \n[ 4 ] - Deletar cliente \n[ 5 ] - Sair e salvar dados\n\n"))
 		
 		if inicio == 1:
 			adicionarCliente()
