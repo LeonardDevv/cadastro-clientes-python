ea5d60982faa12bfd938ac38b5692f0811a215e0
Leonardo Nascimento <leonardofernandespro@gmail.com>
2025-07-07 09:22:32 -0300

    Meu primeiro commit

---
+++ index.py
@@ -0,0 +1,69 @@
+"""
+cadastro-clientes-python
+"""
+
+clientes = []
+id_atual = 1
+
+def adicionarCliente():
+	global id_atual
+	nome_cliente = input("Informe o nome do novo cliente: ")
+	idade_cliente = int(input("Informe a idade do novo cliente: "))
+	email_cliente = input("Informe o e-mail do novo cliente: ")
+	
+	novo_cliente = {
+	"Nome": nome_cliente,
+	"Idade": idade_cliente,
+	"E-mail": email_cliente,
+	"Id": id_atual,
+	"Status": False
+	}
+	clientes.append(novo_cliente)
+	id_atual += 1
+	
+	
+def listarClientes():
+	print(f"{'=' * 30}\nLISTA DE CLIENTES CADASTRADOS\n{'=' * 30}")
+	for cliente in clientes:
+		print(cliente)
+		
+def atualizarCliente():
+	id = int(input("Informe o número do cliente a ser atualizado: "))
+	for cliente in clientes:
+		if cliente["Id"] == id:
+			novo_nome = input("Informe o nome do cliente: ")
+			nova_idade = int(input("Informe a idade do cliente: "))
+			novo_email = input("Informe o E-mail do cliente: ")
+			cliente["Nome"] = novo_nome
+			cliente["Idade"] = nova_idade
+			cliente["E-mail"] = novo_email
+			
+		
+		
+def deletarCliente():
+	global clientes
+	id = int(input("Informe o número do cliente a ser deletado: "))
+	for cliente in clientes:
+		if cliente["Id"] == id:
+			cliente["Status"] = True
+			print(f"Cliente {cliente['Nome']} do E-mail: {cliente['E-mail']} deletado.")
+	clientes = [cliente for cliente in clientes if not cliente["Status"]]
+	
+	
+	
+def iniciar():
+	
+	while True:
+		inicio = int(input(f"Escolha uma opção abaixo: \n\n[ 1 ] - Adicionar cliente \n[ 2 ] - Listar clientes \n[ 3 ] - Atualizar dados do cliente \n[ 4 ] - Deletar cliente \n\n"))
+		
+		if inicio == 1:
+			adicionarCliente()
+		elif inicio == 2:
+			listarClientes()
+		elif inicio == 3:
+			atualizarCliente()
+		elif inicio == 4:
+			deletarCliente()
+
+
+iniciar()
